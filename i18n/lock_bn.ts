7ffb850ef0884132671522a0a73bfb8ce12b29d8
Bhuiyan Mohammad Iklash <bhuiyan.mohammad.iklash@gmail.com>
2021-08-13 18:50:45 +0600

    Fix end of a sentence

--- i18n/lock_bn.ts
+++ i18n/lock_bn.ts
@@ -5,18 +5,18 @@
     <name>LockPrompt</name>
     <message>
         <source>LockPrompt</source>
-        <translation>আপনার পাসওয়ার্ড লিখে রিটার্ন চাঁপুন</translation>
+        <translation>আপনার পাসওয়ার্ড লিখে রিটার্ন চাঁপুন।</translation>
     </message>
     <message>
         <source>LockAttempt</source>
-        <translation>* দুঃখিত, স্ক্রিন আনলক করার ভুল চেষ্টা করেছেন, আবার চেষ্টা করেন</translation>
+        <translation>* দুঃখিত, স্ক্রিন আনলক করার ভুল চেষ্টা করেছেন, আবার চেষ্টা করেন।</translation>
     </message>
 </context>
 <context>
     <name>LockScreen</name>
     <message>
         <source>LockMessage</source>
-        <translation>যে কোনো কী চাপুন বা মাউস একটু সরান</translation>
+        <translation>যে কোনো কী চাপুন বা মাউস একটু সরান।</translation>
     </message>
 </context>
 </TS>
